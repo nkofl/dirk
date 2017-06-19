c6d8908cb30d5e6b59cc5128cbace58ad1658cbd
Callum Macrae <callum@samknows.com>
2017-06-19 16:20:30 +0100

    scaffolding

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>vue dashboard</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+  };
+</script>
+
+<style>
+  body {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  h1, h2 {
+    font-weight: normal;
+  }
+</style>
